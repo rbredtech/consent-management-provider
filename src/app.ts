ecec1c66564591e73d8ba094166e5491183a9199
Manuel Gutjahr <manuel@mgut.at>
2020-10-30 12:17:57 +0100

    Cleanup of logging & env configuration

--- src/app.ts
+++ src/app.ts
@@ -4,21 +4,23 @@ import { join } from 'path';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
-import getenv from 'getenv';
+import * as dotenv from 'dotenv';
 import { createLogger, format, transports } from 'winston';
 
 const { combine, timestamp, printf } = format;
+const tcfApiJsTemplate = readFileSync(join(__dirname, '..', '/templates/mini-cmp.js.template')).toString()
 
-const HTTP_PORT = getenv.int('HTTP_PORT', 3000);
-const COOKIE_DOMAIN = getenv('HTTP_HOST', 'rb.test');
-const COOKIE_NAME = getenv('COOKIE_NAME', 'xconsent');
+dotenv.config();
+const HTTP_PORT = process.env.HTTP_PORT;
+const COOKIE_DOMAIN = process.env.HTTP_HOST;
+const COOKIE_NAME = process.env.COOKIE_NAME  || 'xconsent';
 
 interface ConsentCookie {
   consent: boolean,
 }
 
 const logger = createLogger({
-  level: getenv('LOG_LEVEL', 'info'),
+  level: process.env.LOG_LEVEL || 'info',
   format: combine(
     timestamp(),
     // eslint-disable-next-line @typescript-eslint/no-shadow
@@ -32,23 +34,27 @@ const app = express();
 app.use(cors());
 app.use(cookieParser());
 app.use((req, _, next) => {
-  logger.info(JSON.stringify(req.cookies));
+  logger.debug(JSON.stringify(req.cookies));
   next();
 });
 
 app.get('/mini-cmp.js', (req, res) => {
   let cookie: ConsentCookie | undefined;
   if (req.cookies[COOKIE_NAME]) {
-    cookie = JSON.parse(Buffer.from(req.cookies[COOKIE_NAME], 'base64').toString());
+    try {
+      cookie = JSON.parse(Buffer.from(req.cookies[COOKIE_NAME], 'base64').toString());
+    } catch(e) {
+      logger.info(`Error parsing cookie ${COOKIE_NAME}`, e)
+    }
   }
-
-  logger.debug(`has cookie? ${cookie !== undefined} has consent? ${cookie?.consent}`);
-  const tcfApi = readFileSync(join(__dirname, '..', '/templates/mini-cmp.js.template'))
-    .toString()
+  logger.debug(`hasCookie=${cookie !== undefined}; hasConsent=${cookie?.consent}`);
+  
+  const tcfApi = tcfApiJsTemplate
     .replace('{{TC_STRING}}', JSON.stringify('tcstr'))
-    .replace(/{{TC_PURPOSE_ONE}}/g, JSON.stringify(!!cookie))
-    .replace(/{{TC_CONSENT}}/g, JSON.stringify(cookie?.consent ?? false));
-  res.setHeader('content-type', 'application/javascript');
+    .replace('{{TC_CONSENT}}', JSON.stringify(cookie?.consent ?? false));
+
+  res.setHeader('Content-Type', 'application/javascript');
+  res.setHeader('Cache-Control', 'no-store');
   res.send(tcfApi);
 });
 
@@ -60,7 +66,7 @@ app.get('/setcookie', (req, res) => {
     maxAge: 31536000,
     domain: COOKIE_DOMAIN,
   });
-  res.setHeader('cache-control', 'no-store');
+  res.setHeader('Cache-Control', 'no-store');
   res.sendStatus(200);
 });
 
@@ -69,7 +75,7 @@ app.get('/removecookie', (req, res) => {
     maxAge: 0,
     domain: COOKIE_DOMAIN,
   });
-  res.setHeader('cache-control', 'no-store');
+  res.setHeader('Cache-Control', 'no-store');
   res.sendStatus(200);
 });
 
