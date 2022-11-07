eda66681504f08393902b2efa7f01b56cfb60471
Andreas Jantschnig <andreas.jantschnig@redbull.com>
2022-11-07 11:04:13 +0100

    Revert "no hardcoded server url in tests"
    This reverts commit 280ec81a25628460a5625f8af4ea89ead5040bf3.

--- test/core.spec.js
+++ test/core.spec.js
@@ -1,63 +1,39 @@
-const {
-  describe,
-  beforeAll,
-  afterAll,
-  test,
-  expect,
-} = require("@jest/globals");
+const { describe, beforeAll, afterAll, test, expect } = require("@jest/globals");
 let puppeteer = require("puppeteer");
-let dotenv = require("dotenv");
 
 let browser, page;
 
-dotenv.config();
-const { HTTP_HOST } = process.env;
-const scriptUrl = `http://${HTTP_HOST}/loader.js`;
+const scriptUrl = `http://127.0.0.1:3000/loader.js`;
 
 beforeAll(async () => {
-  browser = await puppeteer.launch({ dumpio: true, args: ["--disable-gpu"] });
-  page = await browser.newPage();
-  page.on("request", (request) => console.log(request.url()));
-  page.on("response", (response) => console.log(response.url()));
+    browser = await puppeteer.launch({dumpio: true, args: ['--disable-gpu']});
+    page = await browser.newPage();
+    page.on('request', request => console.log(request.url()));
+    page.on('response', response => console.log(response.url()));
 }, 20000);
 
+
 afterAll(async () => {
-  await browser.close();
+    await browser.close();
 }, 20000);
 
 describe("Consent Management", () => {
-  let isLoaded;
+    let isLoaded;
 
-  beforeAll(async () => {
-    isLoaded = page.waitForResponse((response) =>
-      response.url().includes("manager-iframe.js")
-    );
-    await page.setContent(
-      `<script type='text/javascript' src="${scriptUrl}"></script>`
-    );
-  });
-
-  describe("Content is loaded", () => {
-    beforeAll(async () => {
-      await isLoaded;
+    beforeAll( async () => {
+        isLoaded = page.waitForResponse(response => response.url().includes('manager-iframe.js'));
+        await page.setContent(`<script type='text/javascript' src="${scriptUrl}"></script>`);
     });
 
-    test("Ping API call returns basic configuration", async () => {
-      const result = page.evaluate(
-        `(new Promise((resolve)=>{window.__tcfapi('ping', 1, resolve)}))`
-      );
+    describe("Content is loaded", () => {
+        beforeAll( async () => {
+            await isLoaded;
+        });
 
-      return expect(result).resolves.toEqual({
-        apiVersion: "2.0",
-        cmpId: 4040,
-        cmpLoaded: true,
-        cmpStatus: "loaded",
-        cmpVersion: 1,
-        displayStatus: "hidden",
-        gdprApplies: true,
-        gvlVersion: 1,
-        tcfPolicyVersion: 2,
-      });
+        test("Ping API call returns basic configuration", async () => {
+            const result = page.evaluate(`(new Promise((resolve)=>{window.__tcfapi('ping', 1, resolve)}))`);
+
+            return expect(result).resolves.toEqual({"apiVersion": "2.0", "cmpId": 4040, "cmpLoaded": true, "cmpStatus": "loaded", "cmpVersion": 1, "displayStatus": "hidden", "gdprApplies": true, "gvlVersion": 1, "tcfPolicyVersion": 2});
+        });
     });
-  });
 });
