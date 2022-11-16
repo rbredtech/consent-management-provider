2ad3cb5ff00d928544022e354f1dae184222f442
Andreas Jantschnig <andreas.jantschnig@redbull.com>
2022-11-16 13:22:32 +0100

    formatting bug

--- src/app.ts
+++ src/app.ts
@@ -155,7 +155,7 @@ const iframeHandler = (req: Request, res: Response) => {
   res.setHeader("Content-Type", "text/html");
   res.setHeader("Cache-Control", "no-store");
   res.render("iframe", {
-    XT: Date.now(), // TODO
+    XT: Date.now(),
     CONSENT_SERVER_HOST: HTTP_HOST,
     URL_SCHEME: req.protocol,
     BANNER: req.withBanner ? "-with-banner" : "",
