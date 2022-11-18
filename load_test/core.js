a20eaf74c5ed39ade0c3be0506fd15a6c1c63719
Daniel Kocsan <daniel.kocsan@tv-insight.com>
2022-11-18 09:41:18 +0100

    Change report to summary

--- load_test/core.js
+++ load_test/core.js
@@ -3,8 +3,8 @@ import {check, sleep} from 'k6';
 
 export const options = {
     thresholds: {
-        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-        http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+        http_req_failed: ['rate<0.00'], // http errors should be less than 1%
+        http_req_duration: ['p(95)<400'], // 95 percent of response times must be below 500ms
     },
 };
 
@@ -12,13 +12,4 @@ export default function() {
     let res = http.get(`http://${__ENV.HTTP_HOST}/${__ENV.API_VERSION}/loader.js`);
     check(res, { 'success': (r) => r.status === 200 });
     sleep(0.3);
-}
-
-export function handleSummary(data) {
-    console.log('Finished executing performance tests');
-
-    return {
-        'stdout': textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout...
-        'summary.json': JSON.stringify(data), // and a JSON with all the details...
-    };
 }
