3367489b7c2a5bbd55c6a474002756b5d905877c
Daniel Kocsan <daniel.kocsan@tv-insight.com>
2022-11-18 09:14:46 +0100

    Change Readme.md Configure CI load test

--- load_test/core.js
+++ load_test/core.js
@@ -1,6 +1,15 @@
 import http from 'k6/http';
 import {check, sleep} from 'k6';
 
+export const options = {
+    duration: '.5m',
+    vus: 50,
+    thresholds: {
+        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+        http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+    },
+};
+
 export default function() {
     let res = http.get(`http://${__ENV.HTTP_HOST}/${__ENV.API_VERSION}/loader.js`);
     check(res, { 'success': (r) => r.status === 200 });
