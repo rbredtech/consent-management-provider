6759b132b54a2a2ff68cb201af1ce5fec76850e4
Daniel Kocsan <daniel.kocsan@tv-insight.com>
2022-11-22 16:47:30 +0100

    Bumb version to 2.1.10

--- test/banner.spec.js
+++ test/banner.spec.js
@@ -19,42 +19,38 @@ afterAll(async () => {
 }, 20000);
 
 describe("Consent Management with banner", () => {
+    let iframe;
     beforeAll(async () => {
-        await page.goto(`http://${pageHelper.HTTP_HOST}/health`);
-        await page.evaluate(`localStorage.setItem("xt", "${Date.now() - 3600000 * 49}");`);
+        await pageHelper.initLoader(page, 0, true);
+        iframe = (await page.frames()).find(frame => frame.url()
+            .indexOf("iframe") > -1);
     });
 
-    describe("Is loaded", () => {
-        let iframe;
+    test("Banner pop-up is NOT displayed", () => {
+        expect(iframe.waitForSelector("div#agttcnstbnnr", {
+            visible: true,
+            timeout: 10
+        }))
+            .rejects
+            .toBeDefined()
+    });
+
+    describe("When showBanner API method is called", () => {
         beforeAll(async () => {
-            await pageHelper.initLoader(page, 0, true);
-            iframe = (await page.frames()).find(frame => frame.url()
-                .indexOf("iframe") > -1);
+            await page.evaluate(`window.__tcfapi('showBanner', 2, console.log)`);
         });
 
-        test("Banner pop-up is NOT displayed", () => {
-            expect(iframe.waitForSelector("div#agttcnstbnnr", {
+        test("Banner pop-up is displayed", async () => {
+            expect(await iframe.waitForSelector("div#agttcnstbnnr", {
                 visible: true,
-                timeout: 10
-            })).rejects.toBeDefined()
+                timeout: 1000
+            }))
+                .toBeDefined();
         });
 
-        describe("When showBanner API method is called", () => {
-            beforeAll(() => {
-                page.evaluate(`(new Promise((resolve)=>{window.__tcfapi('showBanner', 2, resolve)}))`);
-            });
-
-            test("Banner pop-up is displayed", async () => {
-                expect(await iframe.waitForSelector("div#agttcnstbnnr", {
-                    visible: true,
-                    timeout: 1000
-                }))
-                    .toBeDefined();
-            });
-
-            test("Channel Name is replaced in legal text", async () => {
-                expect(await iframe.$eval("div#agttcnstbnnr", node => node.innerText)).toContain("ServusTV");
-            });
+        test("Channel Name is replaced in legal text", async () => {
+            expect(await iframe.$eval("div#agttcnstbnnr", node => node.innerText))
+                .toContain("ServusTV");
         });
     });
 });
