e93c345774f8022c81795bcaf1254f536b903762
Daniel Kocsan <daniel.kocsan@tv-insight.com>
2022-11-25 13:48:19 +0100

    Alter sample rate conditions

--- spec/sample-rate.spec.ts
+++ spec/sample-rate.spec.ts
@@ -5,6 +5,8 @@ process.env.HTTP_HOST = "localhost:3000";
 process.env.HTTP_PROTOCOL = "http";
 process.env.API_VERSION = "v2";
 process.env.COOKIE_DOMAIN = "localhost";
+process.env.CMP_ENABLED_SAMPLING_THRESHOLD_PERCENT = "0";
+process.env.TECH_COOKIE_MIN = "120";
 
 let request = require("supertest");
 const express = require("express");
@@ -22,7 +24,7 @@ beforeAll(() => {
     app.use(`/${process.env.API_VERSION}`, router);
 });
 
-describe("Consent Management API", () => {
+describe("Consent Management API configured with 0% sample rate", () => {
     describe("When called initially", () => {
         let response: Response;
 
@@ -32,9 +34,12 @@ describe("Consent Management API", () => {
         });
 
         test("cmpStatus is disabled", () => {
-            expect(response.status).toBe(200);
             expect(response.text).toContain("eventStatus: 'tcloaded',\n        cmpStatus: 'disabled'");
         });
+
+        test("consent is undefined", () => {
+            expect(response.text).toContain("var hasConsent = undefined;");
+        })
     });
 
     describe("When called with consent decision true", () => {
@@ -46,9 +51,12 @@ describe("Consent Management API", () => {
         });
 
         test("cmpStatus is loaded", () => {
-            expect(response.status).toBe(200);
             expect(response.text).toContain("eventStatus: 'tcloaded',\n        cmpStatus: 'loaded'");
         });
+
+        test("consent is true", () => {
+            expect(response.text).toContain("var hasConsent = true;");
+        })
     });
 
     describe("When called with consent decision false", () => {
@@ -56,27 +64,66 @@ describe("Consent Management API", () => {
 
         beforeAll(async () => {
             response = await request(app)
-                .get("/v2/manager.js?consent=true");
+                .get("/v2/manager.js?consent=false");
         });
 
         test("cmpStatus is loaded", () => {
-            expect(response.status).toBe(200);
             expect(response.text).toContain("eventStatus: 'tcloaded',\n        cmpStatus: 'loaded'");
         });
+
+        test("consent is false", () => {
+            expect(response.text).toContain("var hasConsent = false;");
+        })
     });
 
-
     describe("When called with consent decision undefined", () => {
-        let responsea: Response;
+        let response: Response;
 
         beforeAll(async () => {
-            responsea = await request(app)
+            response = await request(app)
                 .get("/v2/manager.js?consent=undefined");
         });
 
-        test("cmpStatus is loaded", () => {
-            expect(responsea.status).toBe(200);
-            expect(responsea.text).toContain("eventStatus: 'tcloaded',\n        cmpStatus: 'disabled'");
+        test("cmpStatus is disabled", () => {
+            expect(response.text).toContain("eventStatus: 'tcloaded',\n        cmpStatus: 'disabled'");
         });
+
+        test("consent is undefined", () => {
+            expect(response.text).toContain("var hasConsent = undefined;");
+        })
+    });
+
+    describe("When called with recent technical cookie", () => {
+        let response: Response;
+
+        beforeAll(async () => {
+            response = await request(app)
+                .get(`/v2/manager.js?xt=${Date.now()-100}`);
+        });
+
+        test("cmpStatus is disabled", () => {
+            expect(response.text).toContain("eventStatus: 'tcloaded',\n        cmpStatus: 'disabled'");
+        });
+
+        test("consent is undefined", () => {
+            expect(response.text).toContain("var hasConsent = undefined;");
+        })
+    });
+
+    describe("When called with old enough technical cookie", () => {
+        let response: Response;
+
+        beforeAll(async () => {
+            response = await request(app)
+                .get(`/v2/manager.js?xt=${Date.now() - 1000000}`);
+        });
+
+        test("cmpStatus is disabled due to sample rate", () => {
+            expect(response.text).toContain("eventStatus: 'tcloaded',\n        cmpStatus: 'disabled'");
+        });
+
+        test("consent is undefined", () => {
+            expect(response.text).toContain("var hasConsent = undefined;");
+        })
     });
 });
