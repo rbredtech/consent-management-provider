18d32551559e288731235d87445007bb52fcf521
Daniel Kocsan <daniel.kocsan@tv-insight.com>
2022-12-01 09:02:00 +0100

    Test coverage

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -6,6 +6,7 @@ const {
     expect
 } = require("@jest/globals");
 const pageHelper = require("./helper/page");
+const { wait } = require("../helper/util");
 let page;
 
 beforeAll(async () => {
@@ -18,67 +19,21 @@ afterAll(async () => {
 }, 20000);
 
 describe("Consent Management API", () => {
-    let first;
-    beforeAll(async () => {
-        const loaderLoaded = page.waitForResponse(response => response.url()
-            .includes('loader.js'));
-        const managerLoaded = pageHelper.initLoader(page);
-        await loaderLoaded;
-        const apiResponse = page.evaluate(`(new Promise((resolve)=>{window.__tcfapi('getTCData', 1, resolve)}))`);
-        first = await Promise.race([managerLoaded, apiResponse])
-    });
 
-    test("Is available", () => {
-        expect(first.url())
-            .toContain("manager-iframe.js");
-    });
-
-    describe("When debug listener is subscribed",() => {
+    describe("When instantly called after loading", () => {
+        let first;
         beforeAll(async () => {
-            await page.evaluate(() => {
-                window.callbackQueue = [];
-
-                function add(params) {
-                    callbackQueue.push(params);
-                };
-
-                window.__tcfapi('onLogEvent', 1, add);
-            });
+            const loaderLoaded = page.waitForResponse(response => response.url()
+                .includes('loader.js'));
+            const managerLoaded = pageHelper.initLoader(page);
+            await loaderLoaded;
+            const apiResponse = page.evaluate(`(new Promise((resolve)=>{window.__tcfapi('getTCData', 1, resolve)}))`);
+            first = await Promise.race([managerLoaded, apiResponse])
         });
 
-        test("Load event is logged", async () => {
-            const queue = await page.evaluate(() => { return window.callbackQueue; });
-            expect(queue).toHaveLength(2);
-            expect(queue[1]).toEqual({"event": "loaded", "parameters": {"type": "iframe"}, "success": true, ts: expect.any(Number)})
-        });
-
-        describe("And API method is called", () => {
-           beforeAll(async () => {
-               const apiResponse = page.evaluate(`(new Promise((resolve)=>{window.__tcfapi('getTCData', 1, resolve)}))`);
-               await apiResponse;
-           });
-
-           test("Activity is logged", async () => {
-              const queue = await page.evaluate(() => { return window.callbackQueue; });
-              expect(queue).toHaveLength(3);
-              expect(queue[2]).toEqual({"event": "TCData", "parameters": {"status": "disabled"}, "success": true, ts: expect.any(Number)})
-           });
-
-            describe("And API method is called again", () => {
-                let consentCookieLoaded;
-
-                beforeAll(async () => {
-                    consentCookieLoaded = page.waitForResponse(response => response.url().includes('set-consent'));
-                    await page.evaluate(`(new Promise((resolve)=>{window.__tcfapi('setConsent', 1, resolve, false)}))`);
-                    await consentCookieLoaded;
-                });
-
-                test("Activity is logged", async () => {
-                    const queue = await page.evaluate(() => { return window.callbackQueue; });
-                    expect(queue).toHaveLength(4);
-                    expect(queue[3]).toEqual({"event": "setConsent", "parameters": {"consent": "false", "localStorageAvailable": true}, "success": true, ts: expect.any(Number)})
-                });
-            });
+        test("Is available", () => {
+            expect(first.url())
+                .toContain("manager-iframe.js");
         });
     });
 });
---
+++ spec/debug-api.spec.js
@@ -0,0 +1,100 @@
+const {
+    describe,
+    beforeAll,
+    test,
+    expect,
+    afterAll
+} = require("@jest/globals");
+const pageHelper = require("./helper/page");
+
+let page;
+
+beforeAll(async () => {
+    page = await pageHelper.get();
+}, 20000);
+
+afterAll(async () => {
+    await page.browser()
+        .close();
+}, 20000);
+
+describe("Debug API", () => {
+
+    beforeAll(async () => {
+        await pageHelper.initLoader(page);
+    });
+
+    describe("When debug listener is subscribed", () => {
+        beforeAll(async () => {
+            const onLogEvent = await page.evaluate(() => {
+                return new Promise(resolve => {
+                    window.callbackQueue = [];
+
+                    function add(params) {
+                        callbackQueue.push(params);
+                        resolve();
+                    };
+
+                    window.__tcfapi('onLogEvent', 2, add);
+                });
+            });
+
+            await onLogEvent;
+        });
+
+        test("Load event is logged", async () => {
+            const queue = await page.evaluate(() => {
+                return window.callbackQueue;
+            });
+            expect(queue).toHaveLength(1);
+            expect(queue[0]).toEqual({
+                    "event": "loaded",
+                    "parameters": { "type": "iframe" },
+                    "success": true,
+                    ts: expect.any(Number)
+            });
+        });
+
+        describe("And API method is called", () => {
+            beforeAll(async () => {
+                const apiResponse = page.evaluate(`(new Promise((resolve)=>{window.__tcfapi('getTCData', 1, resolve)}))`);
+                await apiResponse;
+            });
+
+            test("Activity is logged", async () => {
+                const queue = await page.evaluate(() => {return window.callbackQueue;});
+                expect(queue).toHaveLength(2);
+                expect(queue[1]).toEqual({
+                    "event": "TCData",
+                    "parameters": { "status": "disabled" },
+                    "success": true,
+                    ts: expect.any(Number)
+                });
+            });
+
+            describe("And API method is called again", () => {
+                let consentCookieLoaded;
+
+                beforeAll(async () => {
+                    consentCookieLoaded = page.waitForResponse(response => response.url().includes('set-consent'));
+                    await page.evaluate(`(new Promise((resolve)=>{window.__tcfapi('setConsent', 1, resolve, false)}))`);
+                    await consentCookieLoaded;
+                });
+
+                test("Activity is logged", async () => {
+                    const queue = await page.evaluate(() => {return window.callbackQueue;});
+                    expect(queue).toHaveLength(3);
+                    expect(queue[2]).toEqual({
+                        "event": "setConsent",
+                        "parameters": {
+                            "consent": "false",
+                            "localStorageAvailable": true
+                        },
+                        "success": true,
+                        ts: expect.any(Number)
+                    });
+                });
+            });
+        });
+    });
+});
