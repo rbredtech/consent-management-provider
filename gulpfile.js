1939e39afcac906846dd4ad60a025ee6fc51202d
Andreas Jantschnig <andreas.jantschnig@redbull.com>
2022-12-13 13:03:35 +0100

    template minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var sourcemaps = require("gulp-sourcemaps");
+var uglify = require("gulp-uglify");
 
 var tsProject = ts.createProject("tsconfig.json");
 
@@ -17,4 +18,26 @@ function copyTemplates() {
   return gulp.src("./src/templates/*").pipe(gulp.dest("./dist/templates"));
 }
 
-exports.default = gulp.series(typescript, copyTemplates);
+function minifyJsTemplates() {
+  return gulp
+    .src("./dist/templates/*.js")
+    .pipe(
+      uglify({
+        compress: {
+          arguments: false,
+          arrows: false,
+          assignments: false,
+          booleans: false,
+          comparisons: false,
+          conditionals: false,
+          evaluate: false,
+          if_return: false,
+          keep_fargs: true,
+          keep_fnames: true,
+        },
+      }),
+    )
+    .pipe(gulp.dest("./dist/templates"));
+}
+
+exports.default = gulp.series(typescript, copyTemplates, minifyJsTemplates);
