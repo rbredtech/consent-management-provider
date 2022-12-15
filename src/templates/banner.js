bf4c25b8498dd8c9f1051d340fe7b148678682b8
Andreas Jantschnig <andreas.jantschnig@gmail.com>
2022-12-15 13:41:51 +0100

    improved banner styles

--- src/templates/banner.js
+++ src/templates/banner.js
@@ -1,15 +1,13 @@
 var inner =
   '<div id="agttcnstbnnr" ' +
-  'style="position:absolute; left:20px; right:20px; bottom:20px; display:none; font-family:sans-serif; font-size:16px; font-weight:400; line-height:24px; color:#505050; background-color:#ffffff; border-radius:8px; border: 4px solid #76b642">' +
+  'style="position:absolute; z-index:9999; left:20px; right:20px; bottom:20px; display:none; font-family:sans-serif; font-size:16px; font-weight:400; line-height:24px; color:#505050; background-color:#ffffff; border-radius:8px; border: 4px solid #76b642">' +
   '<div style="margin: 30px 70px 0;"><span style="display:block;font-size:24px;line-height:24px;font-weight:500;color:#76b642;margin-bottom:16px">Datenschutzeinwilligung zur Reichweitenmessung</span>' +
   '<span>Der Verein Arbeitsgemeinschaft Teletest (kurz AGTT; Details siehe agtt.at/hbb-Messung)<% if(!IS_PRO7){ %>, deren Mitglied <b><%-CHANNEL_NAME%></b> ist<%}%>, möchte das Nutzungsverhalten der Zuseher:innen erfassen, um dadurch den Mitgliedern der AGTT die Möglichkeit zu geben, deren TV Angebot stetig verbessern zu können. Dazu benötigt die AGTT Ihre Zustimmung, nach der ein Cookie auf Ihrem Gerät platziert wird, um folgende Informationen auslesen zu können:<br />' +
   'Geräte ID, IP-Adresse, System- bzw. Browserinformationen, Geräteinformationen (verwendet HbbTV-Version, TV-Hersteller, Übertragungsweg via Satellit oder Kabel, Geräteauflösung). ' +
   'Nähere Informationen zum Datenschutz finden Sie in der HbbTV Applikation des Senders, wo Sie den Status Ihrer Einwilligung verwalten bzw. ändern können.</span></div>' +
   '<div style="color: #76b642; margin: 30px 70px 40px;">' +
-  '<span id="consBtnAgree" class="selected" ' +
-  'style="margin-right: 10px; padding: 10px; border: 3px solid #76b642; border-radius: 8px">Zustimmen</span>' +
-  '<span id="consBtnDismiss" ' +
-  'style="margin-right: 10px;padding: 10px;border: 1px solid #76b642; border-radius: 8px">Ablehnen</span>' +
+  '<span id="consBtnAgree" class="selected" style="margin-right: 10px; padding: 10px; border: 1px solid #76b642; border-radius: 8px; background-color: #76b642; color: #ffffff">Zustimmen</span>' +
+  '<span id="consBtnDismiss" style="margin-right: 10px;padding: 10px;border: 1px solid #76b642; border-radius: 8px">Ablehnen</span>' +
   '</div>' +
   '</div>';
 
@@ -43,16 +41,20 @@ function handleVK(keyCode) {
       // toggle selected button
       if (!consBtnAgree.classList.contains('selected')) {
         consBtnAgree.classList.add('selected');
-        consBtnAgree.style.border = '3px solid #76b642';
+        consBtnAgree.style.color = '#ffffff';
+        consBtnAgree.style.backgroundColor = '#76b642';
 
         consBtnDismiss.classList.remove('selected');
-        consBtnDismiss.style.border = '1px solid #76b642';
+        consBtnDismiss.style.color = '#76b642';
+        consBtnDismiss.style.backgroundColor = '#ffffff';
       } else {
         consBtnAgree.classList.remove('selected');
-        consBtnAgree.style.border = '1px solid #76b642';
+        consBtnAgree.style.color = '#76b642';
+        consBtnAgree.style.backgroundColor = '#ffffff';
 
         consBtnDismiss.classList.add('selected');
-        consBtnDismiss.style.border = '3px solid #76b642';
+        consBtnDismiss.style.color = '#ffffff';
+        consBtnDismiss.style.backgroundColor = '#76b642';
       }
       break;
     default:
