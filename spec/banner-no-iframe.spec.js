afdfc86b192a918052da67006507c9a86bca9522
Daniel Kocsan <daniel.kocsan@tv-insight.com>
2022-12-20 16:05:30 +0100

    TVI-507: Add coverage for banner with no-iframe

---
+++ spec/banner-no-iframe.spec.js
@@ -0,0 +1,102 @@
+const { describe, beforeAll, afterAll, test, expect } = require("@jest/globals");
+const pageHelper = require("./helper/page");
+let page;
+
+beforeAll(async () => {
+  page = await pageHelper.get();
+}, 20000);
+
+afterAll(async () => {
+  await page.browser().close();
+}, 20000);
+
+describe("Consent Management with banner", () => {
+  beforeAll(async () => {
+    await page.setUserAgent("useragent Presto1.2 version");
+    await page.goto(`${pageHelper.HTTP_PROTOCOL}://${pageHelper.HTTP_HOST}/health`);
+    await pageHelper.initLoader(page, 0, true);
+  });
+
+  test("Banner pop-up is NOT displayed", async () => {
+    await expect(
+      page.waitForSelector("div#agttcnstbnnr", {
+        visible: true,
+        timeout: 1000,
+      }),
+    ).rejects.toBeDefined();
+  });
+
+  describe("When showBanner API method is called", () => {
+    beforeAll(async () => {
+      await page.evaluate(function () {
+        window.__cbapi("showBanner", 2, function (consentDecision) {
+          if (typeof consentDecision === "boolean") {
+            window.__tcfapi("setConsent", 2, undefined, consentDecision);
+          }
+        });
+      });
+    });
+
+    test("Banner pop-up is displayed", async () => {
+      await expect(
+        page.waitForSelector("div#agttcnstbnnr", {
+          visible: true,
+          timeout: 1000,
+        }),
+      ).resolves.toBeDefined();
+    });
+
+    test("Channel specific information should be present", async () => {
+      const bannerText = await page.$eval("div#agttcnstbnnr", (node) => node.innerText);
+      expect(bannerText).toContain("deren Mitglied");
+    });
+
+    test("Channel Name is replaced in legal text", async () => {
+      const bannerText = await page.$eval("div#agttcnstbnnr", (node) => node.innerText);
+      expect(bannerText).toContain("ServusTV");
+    });
+
+    describe("When OK button is hit", () => {
+      let consentSent;
+
+      beforeAll(async () => {
+        consentSent = page.waitForRequest((request) => request.url().includes("set-consent"));
+        await page.evaluate(function () {
+          window.__cbapi("handleKey", 2, undefined, 13);
+        });
+      });
+
+      test("Consent is sent", async () => {
+        expect((await consentSent).url()).toContain("set-consent?consent=1");
+      });
+
+      describe("When banner is requested again", () => {
+        beforeAll(async () => {
+          await page.evaluate(function () {
+            window.__cbapi("showBanner", 2, function (consentDecision) {
+              if (typeof consentDecision === "boolean") {
+                window.__tcfapi("setConsent", 2, undefined, consentDecision);
+              }
+            });
+          });
+        });
+
+        describe("And Dismiss is selected", () => {
+          let consentSent;
+
+          beforeAll(async () => {
+            consentSent = page.waitForRequest((request) => request.url().includes("set-consent"));
+            await page.evaluate(() => {
+              window.__cbapi("handleKey", 2, console.log, 37);
+              window.__cbapi("handleKey", 2, console.log, 13);
+            });
+          });
+
+          test("Consent revoke is sent", async () => {
+            expect((await consentSent).url()).toContain("set-consent?consent=0");
+          });
+        });
+      });
+    });
+  });
+});
