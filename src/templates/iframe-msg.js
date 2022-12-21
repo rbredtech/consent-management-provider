3901caac1263d03c0c2b7421960d328711105a65
Andreas Jantschnig <andreas.jantschnig@gmail.com>
2022-12-21 12:48:00 +0100

    refactor: remove unnecessary param from iframe messaging

--- src/templates/iframe-msg.js
+++ src/templates/iframe-msg.js
@@ -11,18 +11,18 @@
       function (event) {
         var message = event.data.split(';');
         if (message[1] === 'cmd') {
+          var id = message[0];
+          var cmd = message[2];
+          var version = message[3];
           var cmdParams = message[4];
           try {
             cmdParams = JSON.parse(cmdParams);
           } catch {}
-          var cmd = message[2];
-          var version = message[3];
-          var id = message[0];
           __tcfapi(
             cmd,
             version,
-            function (r, s) {
-              _message(id + ';' + btoa(JSON.stringify(r)) + (s ? ';1' : ''));
+            function (param) {
+              _message(id + ';' + btoa(JSON.stringify(param)));
             },
             cmdParams,
           );
