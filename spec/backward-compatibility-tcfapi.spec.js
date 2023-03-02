d24bc749ec855b0d5cba0ebe0d919f8781a74400
Andreas Jantschnig <andreas.jantschnig@tv-insight.com>
2023-03-02 12:19:07 +0100

    add backward compatibility test

---
+++ spec/backward-compatibility-tcfapi.spec.js
@@ -0,0 +1,42 @@
+const { describe, beforeAll, afterAll, test, expect } = require("@jest/globals");
+const pageHelper = require("./helper/page");
+let page;
+
+beforeAll(async () => {
+  page = await pageHelper.get();
+}, 20000);
+
+afterAll(async () => {
+  await page.browser().close();
+}, 20000);
+
+describe("Backward compatibility", () => {
+  beforeAll(async () => {
+    await page.goto(`${pageHelper.HTTP_PROTOCOL}://${pageHelper.HTTP_HOST}/health`);
+    await pageHelper.initLoader(page);
+  });
+
+  test("Ping API call returns basic configuration", async () => {
+    const result = page.evaluate(`(new Promise((resolve)=>{window.__tcfapi('ping', 1, resolve)}))`);
+
+    return expect(result).resolves.toEqual({
+      apiVersion: "2.0",
+      cmpId: 4040,
+      cmpLoaded: true,
+      cmpStatus: "loaded",
+      cmpVersion: 1,
+      displayStatus: "hidden",
+      gdprApplies: true,
+      gvlVersion: 1,
+      tcfPolicyVersion: 2,
+    });
+  });
+
+  test("Storage status is disabled", async () => {
+    const apiResponse = await page.evaluate(`(new Promise((resolve)=>{window.__tcfapi('getTCData', 1, resolve)}))`);
+
+    expect(apiResponse.cmpStatus).toBe("disabled");
+    expect(apiResponse.vendor["consents"]).toBeDefined();
+    expect(apiResponse.vendor["consents"]["4040"]).toBeUndefined();
+  });
+});
