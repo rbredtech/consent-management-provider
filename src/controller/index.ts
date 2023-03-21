762322139ea46da545201c5f6c842f6758dac1d2
Andreas Jantschnig <andreas.jantschnig@tv-insight.com>
2023-03-21 09:05:58 +0100

    remove /cmp-with-tracking.js endpoint

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,5 +1,4 @@
 export { bannerController } from "./banner";
-export { cmpWithTrackingController } from "./cmpWithTracking";
 export { cmpController } from "./cmp";
 export { cmpapiController } from "./cmpapi";
 export { cmpapiIframeController } from "./cmpapiIframe";
