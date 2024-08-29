092f0521ea3e8abc2cc0e258266c84bf1edb7f01
Andreas Jantschnig <andreas.jantschnig@tv-insight.com>
2024-08-29 10:05:07 +0200

    remove unnecessary cmpapi-iframe.js endpoint

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -2,7 +2,6 @@ export { bannerController } from "./banner";
 export { cmpWithTrackingController } from "./cmpWithTracking";
 export { cmpController } from "./cmp";
 export { cmpapiController } from "./cmpapi";
-export { cmpapiIframeController } from "./cmpapiIframe";
 export { iframeController } from "./iframe";
 export { setConsentController } from "./setConsent";
 export { removeConsentController } from "./removeConsent";
--- src/router.ts
+++ src/router.ts
@@ -10,7 +10,6 @@ import {
   removeConsentController,
   setConsentController,
   cmpapiController,
-  cmpapiIframeController,
   resetOldConsentController,
 } from "./controller";
 
@@ -27,11 +26,10 @@ router.use(channelMiddleware);
 router.get("/banner.js", bannerController);
 router.get("/cmp.js", cmpController);
 router.get("/cmp-with-tracking.js", cmpWithTrackingController);
+router.get("/cmpapi.js", cmpapiController);
 router.get("/iframe.html", iframeController);
 router.get("/remove-consent", removeConsentController);
 router.get(["/set-consent", "/migrate"], setConsentController);
-router.get("/cmpapi.js", cmpapiController);
-router.get("/cmpapi-iframe.js", cmpapiIframeController);
 router.get("/reset-old-consent", resetOldConsentController);
 
 export default router;
