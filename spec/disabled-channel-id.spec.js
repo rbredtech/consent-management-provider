3a3a066ef618cfc61786328f8b0f0873d1d76984
Andreas Jantschnig <andreas.jantschnig@tv-insight.com>
2024-10-10 12:41:51 +0200

    improved test setup

--- spec/disabled-channel-id.spec.js
+++ spec/disabled-channel-id.spec.js
@@ -1,22 +1,20 @@
 const { describe, beforeAll, afterAll, test, expect } = require("@jest/globals");
 const pageHelper = require("./helper/page");
 const { HTTP_PROTOCOL } = require("./helper/page");
-let page;
-
-beforeAll(async () => {
-  page = await pageHelper.get();
-}, 20000);
-
-afterAll(async () => {
-  await page.browser().close();
-}, 20000);
 
 describe("Consent Management disabled by channel ID", () => {
   describe("loading with disabled channel ID", () => {
+    let page;
+
     beforeAll(async () => {
+      page = await pageHelper.get();
       await page.goto(`${HTTP_PROTOCOL}://${pageHelper.HTTP_HOST}/health`);
       await pageHelper.initLoader(page, 1234);
     });
+
+    afterAll(async () => {
+      await page.browser().close();
+    }, 20000);
 
     test("Storage status is disabled", async () => {
       const apiResponse = await page.evaluate(() => {
@@ -29,10 +27,17 @@ describe("Consent Management disabled by channel ID", () => {
   });
 
   describe("loading with not disabled channel ID", () => {
+    let page;
+
     beforeAll(async () => {
+      page = await pageHelper.get();
       await page.goto(`${HTTP_PROTOCOL}://${pageHelper.HTTP_HOST}/health`);
       await pageHelper.initLoader(page, 9999);
     });
+
+    afterAll(async () => {
+      await page.browser().close();
+    }, 20000);
 
     test("Storage status is loaded", async () => {
       const apiResponse = await page.evaluate(() => {
