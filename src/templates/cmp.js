6987569bfa7fc3e7194422b56afe231979c6ac1b
Andreas Jantschnig <andreas.jantschnig@tv-insight.com>
2024-10-21 15:35:31 +0200

    TEST - add timestamp to url for iframe.html and cmpapi.js

--- src/templates/cmp.js
+++ src/templates/cmp.js
@@ -90,8 +90,9 @@
     iframe.setAttribute(
       'src',
       window.location.protocol +
-        '//<%-CONSENT_SERVER_HOST%>/<%-API_VERSION%>/iframe.html' +
-        (channelId !== '' ? '?channelId=' + channelId : '')
+        '//<%-CONSENT_SERVER_HOST%>/<%-API_VERSION%>/iframe.html?t=' +
+        Date.now() +
+        (channelId !== '' ? '&channelId=' + channelId : '')
     );
     iframe.setAttribute('style', 'position:fixed;border:0;outline:0;top:-999px;left:-999px;width:0;height:0;');
     iframe.setAttribute('frameborder', '0');
@@ -159,8 +160,9 @@
     cmpapiScriptTag.setAttribute(
       'src',
       window.location.protocol +
-        '//<%-CONSENT_SERVER_HOST%>/<%-API_VERSION%>/cmpapi.js?' +
-        (channelId !== '' ? '?channelId=' + channelId : '')
+        '//<%-CONSENT_SERVER_HOST%>/<%-API_VERSION%>/cmpapi.js?t=' +
+        Date.now() +
+        (channelId !== '' ? '&channelId=' + channelId : '')
     );
 
     cmpapiScriptTag.onload = function () {
