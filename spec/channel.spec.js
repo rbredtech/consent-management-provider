d2941e0d4120a67523baa42f646bb38d3fc78525
Andreas Jantschnig <andreas.jantschnig@tv-insight.com>
2024-12-13 12:34:40 +0100

    improved test cases
    execute the tests with all combinations of localStorage and iFrame available or unavailable

--- spec/channel.spec.js
+++ spec/channel.spec.js
@@ -1,41 +1,56 @@
 const { describe, beforeAll, afterAll, test, expect } = require("@jest/globals");
 const pageHelper = require("./helper/page");
-let page;
 
-beforeAll(async () => {
-  page = await pageHelper.get();
-}, 20000);
+const cases = [
+  [true, true], // [localStorage, iFrame]
+  [true, false],
+  [false, true],
+  [false, false],
+];
 
-afterAll(async () => {
-  await page.browser().close();
-}, 20000);
+describe.each(cases)(
+  "Consent Management with ServusTv channel ID - localStorage: %s, iFrame: %s",
+  (localStorage, iFrame) => {
+    let page, iframeResponse, cmpapiResponse;
 
-describe("Consent Management with ServusTv channelId", () => {
-  let iframeResponse, cmpapiResponse;
+    beforeAll(async () => {
+      page = await pageHelper.get(!localStorage, !iFrame);
+      await page.goto(`${pageHelper.HTTP_PROTOCOL}://${pageHelper.HTTP_HOST}/health`);
+      if (iFrame) {
+        iframeResponse = page.waitForResponse((response) => response.url().includes("iframe.html"));
+      }
+      cmpapiResponse = page.waitForResponse((response) => response.url().includes("cmpapi"));
+      await pageHelper.initLoader(page, 0);
+    }, 5000);
 
-  beforeAll(async () => {
-    await page.goto(`${pageHelper.HTTP_PROTOCOL}://${pageHelper.HTTP_HOST}/health`);
-    iframeResponse = page.waitForResponse((response) => response.url().includes("iframe.html"));
-    cmpapiResponse = page.waitForResponse((response) => response.url().includes("cmpapi"));
-    await pageHelper.initLoader(page, 0);
-  });
+    afterAll(async () => {
+      await page.browser().close();
+    }, 5000);
 
-  test("Passing through channelId for iframe request", async () => {
-    expect((await iframeResponse).url()).toContain("channelId=0");
-  });
+    if (iFrame) {
+      test("Passing through channelId for iframe request", async () => {
+        expect((await iframeResponse).url()).toContain("channelId=0");
+      });
+    }
 
-  test("Passing through channelId for cmpapi request", async () => {
-    expect((await cmpapiResponse).url()).toContain("channelId=0");
-  });
-
-  describe("When consent is set", () => {
-    let setConsentResponse = beforeAll(async () => {
-      setConsentResponse = page.waitForResponse((response) => response.url().includes("set-consent"));
-      await page.evaluate(`(new Promise((resolve)=>{window.__cmpapi('setConsent', 2, resolve, true);}))`);
+    test("Passing through channelId for cmpapi request", async () => {
+      expect((await cmpapiResponse).url()).toContain("channelId=0");
     });
 
-    test("Passing through channelId for setConsent request", async () => {
-      expect((await setConsentResponse).url()).toContain("channelId=0");
+    describe("When consent is set", () => {
+      let setConsentResponse = beforeAll(async () => {
+        setConsentResponse = page.waitForResponse((response) => response.url().includes("set-consent"));
+        await page.evaluate(
+          () =>
+            new Promise((resolve) => {
+              window.__cmpapi("setConsent", 2, resolve, true);
+            }),
+        );
+      });
+
+      test("Passing through channelId for setConsent request", async () => {
+        expect((await setConsentResponse).url()).toContain("channelId=0");
+      });
     });
-  });
-});
+  },
+);
